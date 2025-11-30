21de93c5006ca9cec5b817508dfdbed7ef1241aa
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-11-30 17:14:11 -0300

    gv added

--- nvim/init.lua
+++ nvim/init.lua
@@ -211,7 +211,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
@@ -382,7 +381,9 @@ require("lazy").setup({
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-
+					map("gv", function()
+						require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
+					end, "[G]oto Definition (vertical split via Telescope)")
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
